7aaf7c2045789bf921a44420505d5b607a43003c
сhupa <hupa@DESKTOP-TEGFQME>
2019-07-29 00:43:31 +0300

    change

--- src/main/java/by/tc/task02/dao/ApplianceDAO.java
+++ src/main/java/by/tc/task02/dao/ApplianceDAO.java
@@ -3,8 +3,7 @@ package by.tc.task02.dao;
 import by.tc.task02.entity.Appliance;
 import by.tc.task02.entity.criteria.Criteria;
 
-public interface ApplianceDAO {
-	
+public interface ApplianceDAO {	
 	//changes
 	<E> Appliance find(Criteria<E> criteria);
 }
